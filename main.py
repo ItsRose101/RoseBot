08013e0f443fe33e0976d303627dc564cb627ef6
ItsRose101 <itsrealroseYT@gmail.com>
2021-05-16 18:01:44 -0400

    Hello, username! added

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ async def on_message(message):
         return
 
     if message.content.startswith('+hello'):
-        await message.channel.send('Hello!')
+        await message.channel.send('Hello {}!'.format(message.author.nick))
 
 
 _TOKEN_FILE = open('tokenfile.txt', 'r')
